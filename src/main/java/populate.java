6bc14651ddf91f78509ac78b9452c54d5a2936ea
Boris <boris9297@yahoo.com>
2018-05-20 09:49:44 -0700

    finish populate date

--- src/main/java/populate.java
+++ src/main/java/populate.java
@@ -3,7 +3,7 @@ import org.json.*;
 
 import java.io.*;
 import java.sql.*;
-import java.util.*;
+import java.sql.Date;
 //import oracle.simplefan.*;
 /**
  * This is a simple example that demostrates how to open a database
@@ -11,10 +11,10 @@ import java.util.*;
  * close a database connection when we are done. You can build your
  * code using this example as a skeleton.
  */
-public class jdbc {
+public class populate {
     static Connection con = null;
     public static void main(String args[]) {
-        jdbc example = new jdbc();
+        populate example = new populate();
         example.run();
 
     }
@@ -23,16 +23,15 @@ public class jdbc {
         try {
             con = openConnection();
             File user_file = new File("/Users/bliang/GoogleDrive/SCU_Courses/COEN280Database/hw/HW3/YelpDataset/yelp_user.json");
-            File bus_file = new File("/Users/bliang/GoogleDrive/SCU_Courses/COEN280Database/hw/HW3/YelpDataset/small_yelp_business.json");
-            File review_file = new File("/Users/bliang/GoogleDrive/SCU_Courses/COEN280Database/hw/HW3/YelpDataset/small_yelp_review.json");
+            File bus_file = new File("/Users/bliang/GoogleDrive/SCU_Courses/COEN280Database/hw/HW3/YelpDataset/yelp_business.json");
+            File review_file = new File("/Users/bliang/GoogleDrive/SCU_Courses/COEN280Database/hw/HW3/YelpDataset/yelp_review.json");
             BufferedReader reader = null;
             try {
-//                insertData("INSERT INTO yelp_user( user_id) values ('jjj')");
                 BufferedReader reader1 = new BufferedReader(new FileReader(user_file));
                 BufferedReader reader2 = new BufferedReader(new FileReader(bus_file));
                 BufferedReader reader3 = new BufferedReader(new FileReader(review_file));
-                User_Builder(reader1);
-                Bus_Builder(reader2);
+//                User_Builder(reader1);
+//                Bus_Builder(reader2);
                 Review_Builder(reader3);
                 reader1.close();
                 reader2.close();
@@ -51,29 +50,16 @@ public class jdbc {
         }
     }
 
-    public static void insertData(String insertStm) throws SQLException {
+    public static void executeQuery(String insertStm) throws SQLException {
         Statement stmt = con.createStatement();
-        stmt.executeUpdate(insertStm);
-        System.out.println("Inserting Data ...");
-        System.out.println(insertStm);
-//        Boolean isResultSet = stmt.execute("select * from yelp_user");
-//        if (isResultSet) {
-//            ResultSet rs = stmt.getResultSet();
-//            System.out.println(rs);
-//            ResultSetMetaData rsmd = rs.getMetaData();
-//            int columnsNumber = rsmd.getColumnCount();
-//            System.out.println(rs.next());
-//            while (rs.next()) {
-//                for (int i = 1; i <= columnsNumber; i++) {
-//                    if (i > 1) System.out.print(" | ");
-//                    System.out.print(rs.getString(i));
-//                }
-//                System.out.println("");
-//            }
-//        }
-        stmt.executeUpdate(insertStm);
+        System.out.println("You want to " + insertStm + " ? Let me see what I can do.");
+        try {
+            stmt.executeUpdate(insertStm);
 
-
+        } catch (SQLException e) {
+            System.out.println("Sorry! Cant do that");
+        }
+        System.out.println("Successful!");
         stmt.close();
     }
     private void dropTable() throws SQLException {
@@ -122,79 +108,87 @@ public class jdbc {
 
     private static void User_Builder(BufferedReader reader) throws Exception {
         String text = null;
-//        insertData("delete from yelp_user");
+        executeQuery("delete from yelp_user");
+        System.out.println("Inserting Data into yelp_user. Please Wait...");
         while ((text = reader.readLine()) != null) {
             JSONObject obj = new JSONObject(text);
+
+            //----------------------------------------------------------------------------
             String yelp_since =     obj.getString("yelping_since");
+            Date date1 = Date.valueOf(yelp_since + "-01");
             Integer votes =         obj.getJSONObject("votes").getInt("funny") +
                     obj.getJSONObject("votes").getInt("useful") +
                     obj.getJSONObject("votes").getInt("cool");
             Integer review_count =  obj.getInt("review_count");//.get("review_count");
-            String name =           obj.getString("name");
+            String name =           obj.getString("name").replace("'", "''");
             String user_id =        obj.getString("user_id");
             Integer num_friends =   obj.getJSONArray("friends").length();
             Double average_stars =  obj.getDouble("average_stars");
 
-            StringBuilder stm = new StringBuilder();
-//            String stm = "";
-            stm.append("INSERT INTO yelp_user ");
-            stm.append("(yelp_since, num_votes, review_count, name, user_id, num_friends, average_star)");
-            stm.append(" values (");
-            stm.append("TO_DATE('");
-            stm.append(yelp_since  + "'" + ", ");
-            stm.append("'YYYY-MM'), ");
-            stm.append("'" + votes + "'" + ", ");
-            stm.append("'" + review_count.toString()+ "'"  + ", ");
-            stm.append("'" + name + "'" + ", ");
-            stm.append("'" + user_id  + "'" + ", ");
-            stm.append("'" + num_friends.toString() + "'" + ", ");
-            stm.append("'" + average_stars.toString()+ "'" );
-            stm.append(")");
-//            System.out.println(stm.toString());
-//            insertData(stm.toString());
+            //----------------------------------------------------------------------------
+            String stm = "INSERT INTO yelp_user values (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(stm);
+            // 7 columns:
+            ps.setDate(1,  date1);
+            ps.setInt(2, votes);
+            ps.setInt(3, review_count);
+            ps.setString(4, name);
+            ps.setString(5, user_id);
+            ps.setInt(6, num_friends);
+            ps.setDouble(7, average_stars);
 
+            //----------------------------------------------------------------------------
+            ps.execute();
+            ps.close();
         }
+
+        System.out.println("You have now inserted All data into yelp_user!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
     private static void Bus_Builder(BufferedReader reader) throws Exception {
 
+        executeQuery("delete from bus");
         String text = null;
+        System.out.println("Inserting Data into bus. Please Wait...");
         while ((text = reader.readLine()) != null) {
             JSONObject obj = new JSONObject(text);
+            //----------------------------------------------------------------------------
             String business_id = obj.getString("business_id");
-            Boolean open = obj.getBoolean("open");
-            String category = obj.getJSONArray("categories").toString();
+            boolean open = obj.getBoolean("open");
+            String openStr = open ? "true" : "false";
+            String categories = obj.getJSONArray("categories").toString();
             String state = obj.getString("state");
             Integer review_count = obj.getInt("review_count");
             String city = obj.getString("city");
             Double stars = obj.getDouble("stars");
-            String name = obj.getString("name").replace("'", "''");
+            String name = obj.getString("name").replace("'", "''").replace(","," ");
             String attributes = obj.getJSONObject("attributes").toString();
-
-            StringBuilder stm = new StringBuilder();
-            stm.append("INSERT INTO bus");
-            stm.append("(business_id, open, category, state, review_count, city, stars, name, attributes)");
-            stm.append("values (");
-            stm.append("'" +business_id + "'" + ", ");
-            stm.append("'" +open.toString() + "'"  + ", ");
-            stm.append("'" +category + "'"  + ", ");
-            stm.append("'" +state + "'"  + ", ");
-            stm.append( review_count.toString() + "'"  + ", ");
-            stm.append("'" +city + "'"  + ", ");
-            stm.append("'" +stars.toString() + "'"  + ", ");
-            stm.append("'" +name + "'"  + ", ");
-            stm.append("'" +attributes + "'");
-            stm.append( ")");
-//            System.out.println(stm.toString());
-            insertData(stm.toString());
+            //----------------------------------------------------------------------------
+            String stm1 = "INSERT INTO bus values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(stm1);
+            // 9 columns:
+            ps.setString(1,business_id);
+            ps.setString(2, openStr);
+            ps.setString(3, categories);
+            ps.setString(4, state);
+            ps.setDouble(5, review_count);
+            ps.setString(6, city);
+            ps.setDouble(7, stars);
+            ps.setString(8, name);
+            ps.setString(9, attributes);
+            //----------------------------------------------------------------------------
+            ps.execute();
+            ps.close();
         }
+        System.out.println("You have now inserted All data into bus!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
     private static void Review_Builder(BufferedReader reader) throws Exception {
-
-
+        executeQuery("delete from review");
         String text = null;
+        System.out.println("Inserting Data into review. Please Wait...");
+        int count = 0;
         while ((text = reader.readLine()) != null) {
             JSONObject obj = new JSONObject(text);
-
+            //----------------------------------------------------------------------------
             Integer num_votes = obj.getJSONObject("votes").getInt("funny") +
                     obj.getJSONObject("votes").getInt("useful") +
                     obj.getJSONObject("votes").getInt("cool");
@@ -202,29 +196,30 @@ public class jdbc {
             String review_id = obj.getString("review_id");
             Double stars = obj.getDouble("stars");
             String publish_date = obj.getString("date");
+            Date date1 = Date.valueOf(publish_date);
             String main_text = obj.getString("text").replace("'", "''");
             String business_id = obj.getString("business_id");
-
-
-            StringBuilder stm = new StringBuilder();
-            stm.append("INSERT INTO review");
-            stm.append("(num_votes, user_id, review_id, stars,"
-                    + " publish_date, main_text, business_id)");
-            stm.append("values (");
-            stm.append(num_votes + ", ");
-            stm.append(user_id + ", ");
-            stm.append(review_id + ", ");
-            stm.append(stars.toString() + ", ");
-            stm.append(publish_date + ", ");
-            stm.append(main_text + ", ");
-            stm.append(business_id + ", ");
-            stm.append(")");
-//            jdbc.insertData(stm.toString());
-
-//            System.out.println(stm.toString());
+            //----------------------------------------------------------------------------
+            String stm1 = "INSERT INTO review values (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(stm1);
+            // 7 columns:
+            ps.setInt(1, num_votes);
+            ps.setString(2, user_id);
+            ps.setString(3, review_id);
+            ps.setDouble(4, stars);
+            ps.setDate(5,  date1);
+            ps.setString(6, main_text);
+            ps.setString(7, business_id);
+            //----------------------------------------------------------------------------
+            count ++;
+            System.out.println(count);
+            ps.execute();
+            ps.close();
         }
+        System.out.println("You have now inserted All data into review!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
+
 
 
 
