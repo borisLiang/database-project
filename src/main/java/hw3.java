59d28e6d4291c9c1ce4c7c1734ea3b05a1d56817
Boris <boris9297@yahoo.com>
2018-05-26 15:23:58 -0700

    first UI

--- src/main/java/hw3.java
+++ src/main/java/hw3.java
@@ -1,28 +1,302 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 
-public class hw3 extends Application {
 
-    Button button;
+import java.sql.*;
+import java.util.*;
+import java.text.MessageFormat;
 
-    public static void main(String[] args) {
-        launch(args);
+/**
+ */
+public class hw3 {
+    Connection con = null;
+    public static void main(String args[]) {
+        hw3 example = new hw3();
+        example.run();
+    }
+    List<String> busList;
+    private void run() {
+
+        ResultSet result = null;
+        try {
+            con = openConnection();
+//            UserScene(false);
+//            BusinessScene(false);
+            String bus_id1 = "58P3oSd8P2QNzHrQmRqQZw";
+            String bus_id2 = "4qcd2_d3h2eJmvUwUbmOFg";
+            String bus_id3 = "6md4A90THda31wTg1bB3hg";
+
+            ReviewScene(bus_id1);
+        } catch (SQLException e) {
+            System.err.println("Errors occurs when communicating with the database server: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("Cannot find the database driver");
+        } finally {
+            // Never forget to close database connection
+            closeConnection(con);
+        }
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the Window");
-        button = new Button();
-        button.setText("Click me");
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+       /**
+     * This method will first clean up the table and then
+     * populate it with new data.
+     * @param con
+     * @throws java.sql.SQLException
+     */
+
+    private void UserScene (Boolean AndOr) {
+        String andOr = AndOr ? "AND" : "OR";
+        String query = "SELECT name, yelp_since, review_count, num_friends, average_star " +
+                "FROM yelp_user U " +
+                "WHERE " +
+                "yelp_since > to_date(''{1}'','''MM-DD-YYYY''') {0} " +
+                "review_count > {2} {0} " +
+                "num_friends > {3} {0} " +
+                "average_star > {4} {0} " +
+                "num_votes > {5}";
+        String inputDate = "01-01-2000";
+        int revCount = 0;
+        int numFriend = 0;
+        Double avgStar = 0.;
+        int numVotes = 0;
+        System.out.println(MessageFormat.format(query, andOr, inputDate, revCount, numFriend, avgStar, numVotes));
+//        return toList(read(con, query));
+    }
+    private void BusinessScene(Boolean AndOr) throws SQLException{
+        String andOr = AndOr ? "INTERSECT" : "UNION";
+        //VB1 Display
+        List<String> VB1out = VB1();
+
+        List<String> VB1in = new LinkedList<String>();
+        VB1in.add(VB1out.get(5));
+        VB1in.add(VB1out.get(9));
+
+        System.out.println(VB1in);
+        BUS1(VB1in, andOr);
+        List<String> VB2out = VB2(VB1in);
+
+        List<String> VB2in = new LinkedList<String>();
+        VB2in.add(VB2out.get(1));
+        VB2in.add(VB2out.get(2));
+        System.out.println(VB2in);
+
+        BUS2(VB2in, andOr);
+        List<String> VB3out = VB3(VB2in);
+//            System.out.println(VB3out);
+//            System.out.println(VB3out.size());
+
+        List<String> VB3in = new LinkedList<String>();
+        VB3in.add(VB2out.get(1));
+        VB3in.add(VB2out.get(2));
+        BUS3(VB3in, andOr);
+
     }
 
+
+    private List<String> VB1() throws SQLException {
+        String query = "select DISTINCT MC.catName from mainCat MC";
+        return toList(read(con, query));
+    }
+    private void BUS1(List<String> inputList, String andOr) throws SQLException {
+//        String andOr = AndOr ? "INTERSECT" : "UNION";
+        StringBuilder query = new StringBuilder();
+//        query.append("(");
+        query.append("CREATE VIEW BUS1 AS ");
+
+        for (String i : inputList){
+            if(i != inputList.get(0)) {
+                query.append(andOr + " ");
+            }
+            query.append(String.format("SELECT DISTINCT MC.business_id " +
+                    "FROM mainCat MC " +
+                    "WHERE MC.catName = '%s' ", i));
+        }
+        try {
+            read(con, "DROP VIEW BUS1");
+        } catch (SQLException e){
+
+        }
+        System.out.println(query);
+        read(con, query.toString());
+    }
+    private List<String> VB2(List<String> inputList) throws SQLException {
+        StringBuilder query = new StringBuilder();
+//      query.append("(");
+        query.append("SELECT DISTINCT SC.subCatName " +
+                "FROM subCat SC " +
+                "WHERE SC.business_id IN ( SELECT business_id " +
+                "FROM bus1)");
+//      query.append(")");
+        System.out.println(query.toString());
+        return toList(read(con, query.toString()));
+    }
+
+    private void BUS2(List<String> inputList, String andOr) throws SQLException {
+//        String andOr = AndOr ? "UNION" : "INTERSECT";
+        StringBuilder query = new StringBuilder();
+//        query.append("(");
+        query.append("CREATE VIEW BUS2 AS ");
+
+        for (String i : inputList){
+            if(i != inputList.get(0)) {
+                query.append(andOr + " ");
+            }
+            query.append(String.format("SELECT DISTINCT SC.business_id " +
+                    "FROM subCat SC " +
+                    "WHERE SC.subCatName = '%s' ", i));
+        }
+
+        query.append("INTERSECT SELECT DISTINCT * " +
+                "FROM BUS1 B1 ");
+        try {
+            read(con, "DROP VIEW BUS2");
+        } catch (SQLException e){
+
+        }
+        System.out.println(query);
+        read(con, query.toString());
+    }
+    private List<String> VB3(List<String> inputList) throws SQLException {
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT DISTINCT A.attributes " +
+                "FROM attr A " +
+                "WHERE A.business_id IN ( SELECT business_id " +
+                "FROM bus2)");
+        System.out.println(query.toString());
+        return toList(read(con, query.toString()));
+    }
+    private void BUS3(List<String> inputList,String andOr) throws SQLException {
+//        String andOr = AndOr ? "UNION" : "INTERSECT";
+        StringBuilder query = new StringBuilder();
+        query.append("CREATE VIEW BUS3 AS ");
+
+        for (String i : inputList){
+            if(i != inputList.get(0)) {
+                query.append(andOr + " ");
+            }
+            query.append(String.format("SELECT DISTINCT A.business_id " +
+                    "FROM attr A " +
+                    "WHERE A.attributes = '%s' ", i));
+        }
+
+        query.append("INTERSECT SELECT DISTINCT * " +
+                "FROM BUS2 B2 ");
+        try {
+            read(con, "DROP VIEW BUS3");
+        } catch (SQLException e){
+        }
+
+        System.out.println(query);
+        read(con, query.toString());
+
+    }
+    private ResultSet read(Connection con, String query) throws SQLException{
+        Statement stmt = con.createStatement();
+        return stmt.executeQuery(query);
+    }
+    private void desc(ResultSet result) throws SQLException {
+        ResultSetMetaData meta = result.getMetaData();
+        for (int col = 1; col <= meta.getColumnCount(); col++) {
+            System.out.println("Column: " + meta.getColumnName(col) +
+                    "\t, Type: " + meta.getColumnTypeName(col));
+        }
+    }
+    private void ReviewScene(String bus_id) {
+        String query = "SELECT user_id " +
+                "FROM REVIEW " +
+                "WHERE business_id = ''{1}'' {0} " +
+                "publish_date > to_date(''{2}'','''MM-DD-YYYY''') {0} " +
+                "publish_date < to_date(''{3}'','''MM-DD-YYYY''') {0} " +
+                "num_votes > {4} {0} " +
+                "stars > {5}";
+        String andOr = "AND";
+        String inputDate1 = "01-01-1999";
+        String inputDate2 = "01-01-2001";
+        int numVotes = 0;
+        Double stars = 0.;
+        System.out.println(MessageFormat.format(query, andOr, bus_id, inputDate1, inputDate2, numVotes, stars ));
+//        return toList(read(con, query));
+    }
+    private List<String> toList(ResultSet result) throws SQLException {
+        ResultSetMetaData meta = result.getMetaData();
+        int tupleCount = 1;
+        List<String> outputList = new LinkedList<String>();
+        while (result.next()) {
+//            System.out.print("Tuple " + tupleCount++ + " : ");
+            for (int col = 1; col <= meta.getColumnCount(); col++) {
+                if (col == 1) {
+                    outputList.add(result.getString(col));
+                }
+//                System.out.print("\"" + result.getString(col) + "\",");
+            }
+//            System.out.println();
+        }
+        return outputList;
+    }
+    private void navigateResult(Connection c) throws SQLException {
+         /*
+         This will create a Statement that return ResultSets which is:
+         1. Scrollablle (can use ResultSet.previous() or ResultSet.absolute())
+         2. Read-only (cannot call ResultSet.updateXXX() to change the content)
+         */
+        Statement stmt = c.createStatement(
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet r = stmt.executeQuery("SELECT * FROM info");
+        ResultSetMetaData meta = r.getMetaData();
+
+        System.out.print("\nFirst Tuple: ");
+        r.next();
+        for (int col = 1; col <= meta.getColumnCount(); col++) {
+            System.out.print("\"" + r.getString(col) + "\",");
+        }
+
+        System.out.print("\nNext Tuple: ");
+        r.next();
+        for (int col = 1; col <= meta.getColumnCount(); col++) {
+            System.out.print("\"" + r.getString(col) + "\",");
+        }
+
+        System.out.print("\nPrev. Tuple: ");
+        r.previous();
+        for (int col = 1; col <= meta.getColumnCount(); col++) {
+            System.out.print("\"" + r.getString(col) + "\",");
+        }
+
+        System.out.print("\n3rd Tuple: ");
+        r.absolute(3);
+        for (int col = 1; col <= meta.getColumnCount(); col++) {
+            System.out.print("\"" + r.getString(col) + "\",");
+        }
+    }
+
+    private Connection openConnection() throws SQLException, ClassNotFoundException {
+        // Load the Oracle database driver
+        DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+   
+          /* 
+          Here is the information needed when connecting to a database 
+          server. These values are now hard-coded in the program. In 
+          general, they should be stored in some configuration file and 
+          read at run time. 
+          */
+        String host = "localhost";
+        String port = "1521";
+        String dbName = "orcl";
+        String userName = "hr";
+        String password = "oracle";
+
+        // Construct the JDBC URL
+        String dbURL = "jdbc:oracle:thin:@" + host + ":" + port + "/" + dbName;
+        return DriverManager.getConnection(dbURL, userName, password);
+    }
+    private void closeConnection(Connection con) {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.err.println("Cannot close connection: " + e.getMessage());
+        }
+    }
 }
+  
--- src/main/java/populate.java
+++ src/main/java/populate.java
@@ -1,6 +1,6 @@
 
 import org.json.*;
-
+import java.util.*;
 import java.io.*;
 import java.sql.*;
 import java.sql.Date;
@@ -27,12 +27,13 @@ public class populate {
             File review_file = new File("/Users/bliang/GoogleDrive/SCU_Courses/COEN280Database/hw/HW3/YelpDataset/yelp_review.json");
             BufferedReader reader = null;
             try {
+
                 BufferedReader reader1 = new BufferedReader(new FileReader(user_file));
                 BufferedReader reader2 = new BufferedReader(new FileReader(bus_file));
                 BufferedReader reader3 = new BufferedReader(new FileReader(review_file));
 //                User_Builder(reader1);
 //                Bus_Builder(reader2);
-                Review_Builder(reader3);
+//                Review_Builder(reader3);
                 reader1.close();
                 reader2.close();
                 reader3.close();
@@ -58,6 +59,7 @@ public class populate {
 
         } catch (SQLException e) {
             System.out.println("Sorry! Cant do that");
+            System.out.println(e);
         }
         System.out.println("Successful!");
         stmt.close();
@@ -114,8 +116,8 @@ public class populate {
             JSONObject obj = new JSONObject(text);
 
             //----------------------------------------------------------------------------
-            String yelp_since =     obj.getString("yelping_since");
-            Date date1 = Date.valueOf(yelp_since + "-01");
+
+            Date date1 = Date.valueOf(obj.getString("yelping_since") + "-01");
             Integer votes =         obj.getJSONObject("votes").getInt("funny") +
                     obj.getJSONObject("votes").getInt("useful") +
                     obj.getJSONObject("votes").getInt("cool");
@@ -144,48 +146,53 @@ public class populate {
 
         System.out.println("You have now inserted All data into yelp_user!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
-    private static void Bus_Builder(BufferedReader reader) throws Exception {
 
+    private static void Bus_Builder(BufferedReader reader) throws Exception {
+        executeQuery("delete from mainCat");
+        executeQuery("delete from subCat");
+        executeQuery("delete from attr");
         executeQuery("delete from bus");
+
         String text = null;
-        System.out.println("Inserting Data into bus. Please Wait...");
+        System.out.println("Inserting Data into bus & mainCat & subCat & attr. Please Wait...");
         while ((text = reader.readLine()) != null) {
             JSONObject obj = new JSONObject(text);
             //----------------------------------------------------------------------------
             String business_id = obj.getString("business_id");
             boolean open = obj.getBoolean("open");
             String openStr = open ? "true" : "false";
-            String categories = obj.getJSONArray("categories").toString();
+            List<Object> categories = obj.getJSONArray("categories").toList();
             String state = obj.getString("state");
             Integer review_count = obj.getInt("review_count");
             String city = obj.getString("city");
             Double stars = obj.getDouble("stars");
             String name = obj.getString("name").replace("'", "''").replace(","," ");
-            String attributes = obj.getJSONObject("attributes").toString();
+            Map<String, Object> attributes = obj.getJSONObject("attributes").toMap();
             //----------------------------------------------------------------------------
-            String stm1 = "INSERT INTO bus values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            String stm1 = "INSERT INTO bus values (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(stm1);
-            // 9 columns:
-            ps.setString(1,business_id);
+            // 7 columns:
+            ps.setString(1, business_id);
             ps.setString(2, openStr);
-            ps.setString(3, categories);
-            ps.setString(4, state);
-            ps.setDouble(5, review_count);
-            ps.setString(6, city);
-            ps.setDouble(7, stars);
-            ps.setString(8, name);
-            ps.setString(9, attributes);
+            ps.setString(3, state);
+            ps.setDouble(4, review_count);
+            ps.setString(5, city);
+            ps.setDouble(6, stars);
+            ps.setString(7, name);
             //----------------------------------------------------------------------------
             ps.execute();
             ps.close();
+            Category_Builder(categories, business_id);
+            Atribute_Builder(attributes, business_id);
         }
         System.out.println("You have now inserted All data into bus!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
+
     private static void Review_Builder(BufferedReader reader) throws Exception {
         executeQuery("delete from review");
         String text = null;
         System.out.println("Inserting Data into review. Please Wait...");
-        int count = 0;
+        Integer count = 0;
         while ((text = reader.readLine()) != null) {
             JSONObject obj = new JSONObject(text);
             //----------------------------------------------------------------------------
@@ -210,13 +217,106 @@ public class populate {
             ps.setDate(5,  date1);
             ps.setString(6, main_text);
             ps.setString(7, business_id);
+
             //----------------------------------------------------------------------------
-            count ++;
-            System.out.println(count);
+//            count ++;
+//            System.out.println(count.toString() + "/826190 review inserted");
             ps.execute();
             ps.close();
         }
         System.out.println("You have now inserted All data into review!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+    private static void Category_Builder(List<Object> categories, String business_id) throws SQLException {
+        Set<String> catSet = new HashSet<>();
+        catSet.add("Active Life");
+        catSet.add("Arts & Entertainment");
+        catSet.add("Automotive");
+        catSet.add("Car Rental");
+        catSet.add("Cafes");
+        catSet.add("Beauty & Spas");
+        catSet.add("Convenience Stores");
+        catSet.add("Dentists");
+        catSet.add("Doctors");
+        catSet.add("Drugstores");
+        catSet.add("Department Stores");
+        catSet.add("Education");
+        catSet.add("Event Planning & Services");
+        catSet.add("Flowers & Gifts");
+        catSet.add("Food");
+        catSet.add("Hardware Stores");
+        catSet.add("Health & Medical");
+        catSet.add("Home Services");
+        catSet.add("Home & Garden");
+        catSet.add("Hospitals");
+        catSet.add("Hotels & Travel");
+        catSet.add("Grocery");
+        catSet.add("Medical Centers");
+        catSet.add("Nurseries & Gardening");
+        catSet.add("Nightlife");
+        catSet.add("Restaurants");
+        catSet.add("Shopping");
+        catSet.add("Transportation");
+
+//        System.out.println("Inserting Data into mainCat & subCat. Please Wait...");
+        for (Object i : categories) {
+            String iStr = i.toString();
+//            System.out.println(iStr);
+            if (catSet.contains(iStr)){
+                String stm = "INSERT INTO mainCat VALUES (?, ?)";
+                PreparedStatement ps = con.prepareStatement(stm);
+                ps.setString(1, iStr);
+                ps.setString(2, business_id);
+                ps.execute();
+                ps.close();
+            } else {
+                String stm = "INSERT INTO subCat VALUES (?, ?)";
+                PreparedStatement ps = con.prepareStatement(stm);
+                ps.setString(1, iStr);
+                ps.setString(2, business_id);
+                ps.execute();
+                ps.close();
+            }
+        }
+
+    }
+
+    private static void Atribute_Builder(Map<String, Object> map, String business_id) throws SQLException{
+//        System.out.println(Arrays.toString(map.entrySet().toArray()));
+        String stm = "INSERT INTO attr VALUES (?, ?)";
+
+        for (String everyKey : map.keySet()) {
+            Object value = map.get(everyKey);
+
+            //attributes has 4 types: int, String, boolean and HashMap
+            // if it is HashMap, dig further
+            // else: toString
+            if (value instanceof HashMap){
+                Map<String, Object> newMap = new HashMap<String, Object>((HashMap)value);
+                for (String everyKey2 : newMap.keySet()) {
+                    Object value1 = newMap.get(everyKey2);
+                    String valueStr = value1.toString();
+                    PreparedStatement ps = con.prepareStatement(stm);
+                    ps.setString(2, business_id);
+                    ps.setString(1, everyKey + " " + everyKey2 + " : " + valueStr);
+
+//                    System.out.println(everyKey + " " + everyKey2 + " : " + valueStr);
+                    ps.execute();
+                    ps.close();
+
+                }
+            } else {
+                String valueStr = value.toString();
+                PreparedStatement ps = con.prepareStatement(stm);
+                ps.setString(2, business_id);
+                ps.setString(1, everyKey + " : " + valueStr);
+//                System.out.println(everyKey + " : " + valueStr);
+                ps.execute();
+                ps.close();
+            }
+        }
+
+
     }
 }
 
